cce586d6060e15daf21e9baf0d8222e3014d67c9
Erskine Gamaliel L. Duenas <erskine.duenas@gmail.com>
2024-10-25 20:15:20 +0800

    10/25/2024
    -learned debounce pattern
    -started API calling
    -started appending div child to html file
    -created utils.js

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "8620aa62",
-      s: "avengers",
+      s: searchTerm,
     },
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
 
+  // console.log(response.data);
+  return response.data.Search;
 };
 
+const input = document.querySelector("input");
 
-fetchData();
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+
+  for (const movie of movies) {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+      <img src="${movie.Poster}"/>
+      <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector("#target").appendChild(div);
+  }
+};
+
+input.addEventListener("input", debounce(onInput, 800));
---
+++ utils.js
@@ -0,0 +1,13 @@
+//this is the pattern where the delay of API calls happen
+//Debouncing pattern helps avoid excessive API calls when users are typing continuously
+const debounce = (func, delay = 1000) => {
+    let timeoutId;
+    return (...args) => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        func.apply(null, args);
+      }, delay);
+    };
+  };
